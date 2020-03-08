1def394e4217e255be5dfd29e1dedd1b758c7dfe
Øyvind Hauge <oyvind.hauge@evry.com>
2020-03-08 16:22:25 +0100

    local search WIP

--- heuristics/localsearch.go
+++ heuristics/localsearch.go
@@ -4,5 +4,8 @@ import "github.com/oyvinddd/inf273/models"
 
 // LocalSearch does a local search on a given solution
 func LocalSearch(solution [][]*models.Call) {
+	//current, best := solution, solution
+	for i := 0; i < 10000; i++ {
 
+	}
 }
